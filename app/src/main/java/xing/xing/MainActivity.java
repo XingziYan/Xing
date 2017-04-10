94c947a1aba9a55afdc04a4eea2cd3e6e7d5c629
XingziYan <2431249320@qq.com>
2017-04-10 11:11:40 +0800

    first commit

--- app/src/main/java/xing/xing/MainActivity.java
+++ app/src/main/java/xing/xing/MainActivity.java
@@ -1,13 +1,16 @@
 package xing.xing;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(mContext,"111",Toast.LENGTH_SHORT).show();
     }
 }
