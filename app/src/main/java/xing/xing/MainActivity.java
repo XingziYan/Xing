990925f50e1b727e00c50006c5aa9cf38d3f8097
XingziYan <2431249320@qq.com>
2017-04-10 10:31:08 +0800

    first commit

---
+++ app/src/main/java/xing/xing/MainActivity.java
@@ -0,0 +1,13 @@
+package xing.xing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
